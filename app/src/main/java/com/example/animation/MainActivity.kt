610ce56dfa4ad70bd0fcf1aff9847198e6558cdd
黃歆芝的實力 <bella12345694@gmail.com>
2024-12-02 14:50:14 +0800

    01

--- app/src/main/java/com/example/animation/MainActivity.kt
+++ app/src/main/java/com/example/animation/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.animation
 
 import android.os.Bundle
+import android.view.animation.Animation
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +17,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.animation.ui.theme.AnimationTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.material3.Button
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +36,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             AnimationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                   /* Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    )*/
+                    Animation( m =  Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +48,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Animation( m: Modifier = Modifier) {
+    var appear by remember { mutableStateOf(true) }  //背景出現
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AnimationTheme {
-        Greeting("Android")
+    Column {
+        Button(
+            onClick = { appear = !appear },
+            modifier = m
+        ) {
+            if (appear) Text(text = "星空背景圖消失")
+            else Text(text = "星空背景圖出現")
+        }
+        AnimatedVisibility(
+            visible = appear,
+            enter = fadeIn(
+                initialAlpha = 0.1f,
+                animationSpec = tween(durationMillis = 5000)),
+            exit = fadeOut(
+                animationSpec = tween(durationMillis = 5000))
+
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.sky),
+                contentDescription = "星空背景圖"
+            )
+        }
+
+
+
+
     }
 }
+
+
+
+
